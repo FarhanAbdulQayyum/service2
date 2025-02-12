c2f7f64aa8991ee2f8127df0c0057bd8e35a5284
FarhanAbdulQayyum <livefarhan1@gmail.com>
2025-02-12 16:26:37 +0500

    qwe

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = 3001;
 
 // Define a simple route
 app.get("/", (req, res) => {
-    res.send('________This is Service 2 try 3');
+    res.send('________This is Service 2 try qwe');
 });
 
 // Start the server
