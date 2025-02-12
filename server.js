31ff3de16065baf8ce9d5541e08ae54dbc1907c5
FarhanAbdulQayyum <livefarhan1@gmail.com>
2025-02-12 15:42:16 +0500

    test github actions

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = 3001;
 
 // Define a simple route
 app.get("/", (req, res) => {
-    res.send('________This is Service 2  after ip fixed');
+    res.send('________This is Service 2  test with git hub');
 });
 
 // Start the server
