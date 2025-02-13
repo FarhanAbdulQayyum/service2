145d20637f4aabc0a20c1aa4fae39a93fe2ba42a
FarhanAbdulQayyum <livefarhan1@gmail.com>
2025-02-13 14:02:22 +0500

    check

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = 3001;
 
 // Define a simple route
 app.get("/", (req, res) => {
-    res.send('________This is Service 2 ________');
+    res.send('________This is Service 2 ________check');
 });
 
 // Start the server
